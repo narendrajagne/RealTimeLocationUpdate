43c334dbd5eed5f1108d6c4a02572aadb28e4927
NK Jagne <nkjagne@NKs-MacBook-Pro.local>
2019-07-28 16:11:53 +0530

    My Frist Commit

---
+++ RealTimeLocationDemo/RealTimeLocationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeLocationDemo
+//
+//  Created by NK Jagne on 28/07/19.
+//  Copyright © 2019 Viztar Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
